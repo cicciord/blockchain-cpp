6115b1a512016509a83f7323358aab2c77880dee
thirdeth <thirdeth@users.noreply.github.com>
2022-11-17 03:46:48 +0100

    header blockchain

---
+++ Blockchain.h
@@ -0,0 +1,22 @@
+#ifndef _BLOCKCHAIN
+#define _BLOCKCHAIN
+
+#include <cstdint>
+#include <vector>
+#include "Block.h"
+using namespace std;
+
+class Blockchain {
+    public:
+        Blockchain();
+
+        void AddBlock( Block newBlock );
+    
+    private:
+        uint32_t _diff;
+        vector<Block> _chain;
+
+        Block _GetLastBlock() const;
+}
+
+#endif
